16e7408d53d2ed98edc368c7b7dbd4db27b843be
Adrien Jussak <jussak.adrien@gmail.com>
2017-03-15 18:42:18 +0100

    Duplication issue fix

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ client = discord.Client()
 async def on_message(message):
     if message.author.id != client.user.id and message.channel.id in config["syncedChannels"] \
             and not message.content.startswith("!"):
+        for embed in message.embeds:
+            message.attachments.append(embed["image"])
+        for object in message.attachments:
+            if message.content != "":
+                message.content += "\n"
+            message.content += object["url"]
         for synced_channel_id in config["syncedChannels"]:
             if synced_channel_id != message.channel.id:
-                for embed in message.embeds:
-                    message.attachments.append(embed["image"])
-                for object in message.attachments:
-                    if message.content != "":
-                        message.content += "\n"
-                    message.content += object["url"]
                 await client.send_message(client.get_channel(synced_channel_id),  message.content)
 
 print("Discord Chat Syncer\n\nBot is running...")
