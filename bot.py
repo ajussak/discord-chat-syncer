5e6e407dda89e8fa277237f8aa72beac149e9257
Adrien Jussak <jussak.adrien@gmail.com>
2017-03-15 19:59:16 +0100

    Added Logging System

--- bot.py
+++ bot.py
@@ -2,9 +2,27 @@
 
 import discord
 import json
+import logging
 
 config = json.load(open("config.json"))
 client = discord.Client()
+
+logger = logging.getLogger("discord-chat-logger")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("bot.log")
+file_handler.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter(fmt='%(asctime)s : %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 
 @client.event
@@ -17,9 +35,18 @@ async def on_message(message):
             if message.content != "":
                 message.content += "\n"
             message.content += object["url"]
+
+        broadcast_list = ""
         for synced_channel_id in config["syncedChannels"]:
             if synced_channel_id != message.channel.id:
-                await client.send_message(client.get_channel(synced_channel_id),  message.content)
+                channel = client.get_channel(synced_channel_id)
+                last_broadcasted = await client.send_message(channel,  message.content)
+                broadcast_list += "\n - {0} (ID:{1}) : Message ID:{2}".format(channel.server.name, channel.server.id,
+                                                                              last_broadcasted.id)
 
-print("Discord Chat Syncer\n\nBot is running...")
+        logger.info("Message ID:%s posted by %s (ID:%s) from server %s (ID:%s) broadcasted to : %s",
+                    message.id, message.author.name, message.author.id, message.server.name, message.server.id,
+                    broadcast_list)
+
+print("Discord Chat Syncer\n")
 client.run(config["token"])
