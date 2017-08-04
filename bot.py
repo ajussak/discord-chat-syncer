4edc12e7041da8aed3d125ab169e25143084f1e4
Adrien Jussak <jussak.adrien@gmail.com>
2017-08-04 16:53:25 +0200

    New configuration format and server owners is now able to define synced channel.

--- bot.py
+++ bot.py
@@ -3,36 +3,48 @@
 import discord
 import json
 import logging
+import os
 
-config = json.load(open("config.json"))
-client = discord.Client()
+from discord import Embed
 
-logger = logging.getLogger("discord-chat-logger")
-logger.setLevel(logging.DEBUG)
-
-file_handler = logging.FileHandler("bot.log")
-file_handler.setLevel(logging.INFO)
-
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter(fmt='%(asctime)s : %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
 logger.addHandler(console_handler)
+
+
+def save_config(config_data):
+    with open('config.json', 'w') as outfile:
+        json.dump(config_data, outfile, indent=4, sort_keys=True)
+
+if not os.path.exists('config.json'):
+    config = {'token': '', 'super_admin_id': '', 'enable_anonymity': False, 'delete_banned_users_messages': True,
+              'text_only_messages_filtering': False, 'synced_channels': [], 'banned_users': []}
+    save_config(config)
+else:
+    config = json.load(open("config.json"))
+
+if config['token'] == '':
+    logger.critical('Configuration file no completed : Discord token is missing')
+    exit(1)
+
+if config['super_admin_id'] == '':
+    logger.warning('The Super Admin is not defined in configuration file. Moderation system disabled.')
+
+logger.info("Starting...")
+client = discord.Client()
 
 
 @client.event
 async def on_message(message):
     if message.author.id != client.user.id and message.channel.id in config["synced_channels"] \
             and not message.content.startswith("!"):
-        if not config["enable_anonymous"]:
+        if not config["enable_anonymity"]:
             message.content = "**{0}@{1}**\n".format(message.author.name, message.channel.server.name) + message.content
-        for embed in message.embeds:
-            message.attachments.append(embed["image"])
         for object in message.attachments:
             if message.content != "":
                 message.content += "\n"
@@ -44,15 +56,23 @@ async def on_message(message):
                 if channel is None:
                     logger.error("Channel ID:%s doesn't exist or the bot is not allowed.", synced_channel_id)
                 else:
-                    send_message = await client.send_message(channel, message.content)
+                    emb = None
+                    if len(message.embeds) >= 1:
+                        emb = Embed(**message.embeds[0])
+                    print(emb.to_dict())
+                    send_message = await client.send_message(channel, message.content, embed=emb)
                     broadcast_list += "\n - {0} (ID:{1}) : Message ID:{2}".format(channel.server.name,
                                                                                   channel.server.id,
                                                                                   send_message.id)
-
         logger.info("Message ID:%s posted by %s (ID:%s) from server %s (ID:%s) broadcasted to : %s",
                     message.id, message.author.name, message.author.id, message.server.name, message.server.id,
                     broadcast_list)
+    elif message.content == '!setsync' and message.author == message.channel.server.owner:
+        for channel in message.channel.server.channels:
+            if channel.id in config["synced_channels"]:
+                config["synced_channels"].remove(channel.id)
+        config["synced_channels"].append(channel.id)
+        save_config(config)
+        await client.send_message(message.channel, "Synchronization enabled here.")
 
-
-print("Discord Chat Syncer\n")
 client.run(config["token"])
